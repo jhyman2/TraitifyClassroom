be9b684a6c0fb648053ed1d39f244dfe42bdb3b1
prod1 <prod1@vbox.umbc.edu>
2014-11-15 14:39:19 -0500

    Update 2:39p

--- server/traitifyclass/traitifyclass/urls.py
+++ server/traitifyclass/traitifyclass/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+import views
 #from tests import urls
 
 urlpatterns = patterns('',
@@ -8,4 +9,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 	url(r'^admin/', include(admin.site.urls)),
 	url(r'^tests/', include('tests.urls', namespace='tests')),
+	url(r'^', views.home),
 )
