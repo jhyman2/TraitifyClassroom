017d67f4f046ab8f19eed4e9355f7359b230ed6b
prod1 <prod1@vbox.umbc.edu>
2014-11-15 17:03:40 -0500

    Django 5:00

--- server/traitifyclass/tests/urls.py
+++ server/traitifyclass/tests/urls.py
@@ -5,4 +5,5 @@ urlpatterns = patterns('tests',
 	url(r'log/$', 'views.log', name='log'),
 	url(r'assess/$', 'views.testDetail', name='test_detail'),
 	url(r'assess/result/$', 'views.testResult', name='test_result'),
+	url(r'generate/(?P<user_count>\d+)/$', 'views.generate', name='generate'),
 )
