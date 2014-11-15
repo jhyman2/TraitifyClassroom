246d1fda0e69f899a9acda366c9c5ef78e35eed2
Jamison Hyman <jhyman2@umbc.edu>
2014-11-15 17:59:18 -0500

    For Joseph

--- traitifyclass/settings.py
+++ traitifyclass/settings.py
@@ -81,5 +81,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
-
+STATIC_ROOT = "sitestatic"
 STATIC_URL = '/static/'
+STATICFILES_DIRS = ['static']
+STATICFILES_FINDERS = (
+   'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+   'django.contrib.staticfiles.finders.FileSystemFinder',
+)
