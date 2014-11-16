d1921242766dd0400bc19a1c58be682bd05747ff
Jamison Hyman <jhyman2@umbc.edu>
2014-11-16 14:21:07 -0500

    Help me Joseph, you're my only hope

--- tests/admin.py
+++ tests/admin.py
@@ -6,10 +6,8 @@ from django.contrib.admin import AdminSite
 class StudentAdmin(admin.ModelAdmin):
 	fields = ['first', 'last', 'test_id', 'finished_test']
 
-admin.site.register(Student)
-
 class MyAdminSite(AdminSite):
     site_header = 'Monty Python administration'
 
 admin_site = MyAdminSite(name='myadmin')
-admin_site.register(Student)
+admin_site.register(Student, StudentAdmin)
--- traitifyclass/urls.py
+++ traitifyclass/urls.py
@@ -1,11 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 import views
-#from tests import urls
+from ../tests/admin import MyAdminSite
 
 urlpatterns = patterns('',
-	url(r'^admin/', include(admin.site.urls)),
+	url(r'^admin/', include(MyAdminSite.urls)),
 	url(r'^tests/', include('tests.urls', namespace='tests')),
 	url(r'^', views.home),
-    url(r'^admin/', include(admin.site.urls)),
 )
