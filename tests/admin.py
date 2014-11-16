548be3aec65adf38ab5f0c9701e5318402c0edf5
prod1 <prod1@vbox.umbc.edu>
2014-11-16 13:25:45 -0500

    1:25 Django

--- tests/admin.py
+++ tests/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from models import Student
 
 class StudentAdmin(admin.ModelAdmin):
-	fields = ['first', 'last', 'test_id', 'finished_test']
+	fields = ('first', 'last', 'finished_test')
+	list_display = ('first', 'last')
 
 admin.site.register(Student)
