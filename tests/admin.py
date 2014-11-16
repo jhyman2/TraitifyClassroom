dfd360bfa35eab5fdb87bccdbfd49442f8ab638a
Jamison Hyman <jhyman2@umbc.edu>
2014-11-16 13:56:15 -0500

    JOSEPH PLS AGAIN

--- tests/admin.py
+++ tests/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from models import Student
+from django.contrib.admin import AdminSite
+
 
 class StudentAdmin(admin.ModelAdmin):
 	fields = ['first', 'last', 'test_id', 'finished_test']
 
 admin.site.register(Student)
+
+class MyAdminSite(AdminSite):
+    site_header = 'Monty Python administration'
+
+admin_site = MyAdminSite(name='myadmin')
+admin_site.register(Student)
