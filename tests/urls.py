c0b9880aaf383bb9dca37de60d272555226fe656
prod1 <prod1@vbox.umbc.edu>
2014-11-16 12:46:07 -0500

    12:46

--- tests/urls.py
+++ tests/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, url
 
-from views import LogView, TestDetailView, GenerateView
+from views import LogView, TestDetailView, GenerateView, PictureView
 
 urlpatterns = patterns('',
 	url(r'log/$', LogView.as_view(), name='log'),
 	url(r'assess/$', TestDetailView.as_view(), name='test_detail'),
+	url(r'pict/$', PictureView.as_view(), name='picture'),
 	url(r'generate/(?P<user_count>\d+)/$', GenerateView.as_view(), name='generate'),
 )
